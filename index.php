0a4ad256a21be2db79d87895c6d467d7c09fb64d
khaleel-132 <khaleel.alungal132@gmail.com>
2020-06-25 01:56:21 +0530

    function added

--- index.php
+++ index.php
@@ -1,23 +1,25 @@
 <?php
-$firstnum = 8;
-$secondnum = 9;
-$operator ="Add";
-$result = '';
+function calculator($firstnum,$secondnum,$operator){
     switch ($operator) {
         case "Add":
            $result = $firstnum + $secondnum;
-           echo $result;
+           echo 'The Result After Addition Is : ' .$result;
             break;
         case "Sub":
            $result = $firstnum - $secondnum;
-           echo $result;
+           echo 'The Result After Substraction Is : ' .$result;
             break;
         case "Mul":
             $result = $firstnum * $secondnum;
-            echo $result;
+            echo 'The Result After Multiplication Is : ' .$result;
             break;
         case "Div":
             $result = $firstnum / $secondnum;
-            echo $result;
+            echo 'The Result After Division Is : ' .$result;
     }
+}
+calculator(100,100,'Add');
+calculator(100,100,'Sub');
+calculator(100,100,'Mul');
+calculator(100,100,'Div');
 ?>
